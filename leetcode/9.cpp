5dc65e3d64b8e105e6a59b478f3f34c20d049e70
aitazhibayeva <tazhibayevaaiza@gmail.com>
2023-12-15 01:21:55 +0600

    leetcode exercises

--- leetcode/9.cpp
+++ leetcode/9.cpp
@@ -9,13 +9,19 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int k=0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]<target){
-                k++;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
             }
         }
-        return k;
+        return left;
     }
 };
 
